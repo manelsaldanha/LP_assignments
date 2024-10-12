c05f4c3abcc832f7b92da8759ecc075701bf0300
manelsaldanha <manuel.saldanha2000@gmail.com>
2024-10-12 17:53:55 +0100

    creating new fixtures

--- life_expectancy/creating_features.py
+++ life_expectancy/creating_features.py
@@ -10,10 +10,10 @@ raw_data = pd.read_csv(PROJECT_DIR / "data" / "eu_life_expectancy_raw.tsv", sep=
 # Creating input fixture
 input_feature = raw_data.sample(frac=0.1, random_state=42)
 
-# Creating expected fixture
+# Creating expected fixture for
 df_clean = clean_data(input_feature)
 expected_feature = df_clean[df_clean["region"] == "PT"]
 
 # Save the sample data as a new fixture
 input_feature.to_csv(PROJECT_DIR / "tests" / "fixtures" / "eu_life_expectancy_raw.tsv", sep='\t', index=False)
-expected_feature.to_csv(PROJECT_DIR / "tests" / "fixtures" / "eu_life_expectancy_expected.csv", index=False)
+expected_feature.to_csv(PROJECT_DIR / "tests" / "fixtures" / "pt_life_expectancy_expected.csv", index=False)
